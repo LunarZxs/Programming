490194bbfebb79353909a11343c3e1b75da35c69
LunarZxs <ranul.jaya@gmail.com>
2019-12-19 00:02:15 +1300

    Python functions

---
+++ Python/ifelif.py
@@ -0,0 +1,12 @@
+#This program prints hello world with if and elif statements
+
+y = 4
+x = 5
+
+if x == 5:
+    x = print('Hello World')
+    if(x != y):
+        print(bool(x))
+
+elif y == 4:
+    print(x)
---
+++ Python/list.py
@@ -0,0 +1,7 @@
+#replecing strings inside a list
+
+x = ["Hello World", "Terrorist"]
+
+x[0] = x[0].replace("e", 'g')
+
+print(x)

1444a3b387ece4971aeaa01579c48e55ecc8e88c
LunarZxs <ranul.jaya@gmail.com>
2020-01-04 21:21:57 +1300

    programming

---
+++ C++/Userinput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int input;
+
+int main()
+{
+    cout << "Please enter a number ";
+    cin >> input;
+
+    cout << "You have entered in " << input << endl;
+
+    return 0;
+}
---
+++ C++/arrays.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+    int Nums[10];
+
+    Nums[0] = 10;
+
+    cout << Nums[0] << endl; 
+
+    return 0;
+}

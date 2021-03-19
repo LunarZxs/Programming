bd7ca7bc4571455fb53095c06ac337b1c646b73e
RanulJaya <ranul.jaya@gmail.com>
2021-03-19 22:29:57 +1300

    formatting strings for the file

---
+++ Python/formattedString.py
@@ -0,0 +1,6 @@
+first_Name = 'Ranul'
+last_Name = 'Jaya'
+
+message = f'{first_Name} {last_Name} is a programmer' #This is a formatted string use f' and then curly braces for the variables
+
+print(message)

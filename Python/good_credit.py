678ed03e8b45b061c1d4366637df5137efea5c58
RanulJaya <ranul.jaya@gmail.com>
2021-03-20 12:10:37 +1300

    down payment for the good_credit.py

---
+++ Python/good_credit.py
@@ -0,0 +1,10 @@
+price = 1000000
+is_good_credit = True
+
+if is_good_credit:
+    down_payment = price * 0.10
+
+else:
+    down_payment = price * 0.20
+
+print(f'The down payment is ${down_payment}0')

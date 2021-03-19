5234d8f2bf0624b3f38a0c136547c41d23788429
RanulJaya <ranul.jaya@gmail.com>
2021-03-20 12:04:12 +1300

    conditions.py

---
+++ Python/condition.py
@@ -0,0 +1,11 @@
+hot_day = False
+cold_day = True
+
+if hot_day:
+    print("It is a hot day")
+
+elif cold_day: #This condition else if will activate if the condition is true
+    print("It is a cold day")
+
+else:
+    print("it is a warm day")

5bdfc2e1610cb0ef98d3a5e9670df5c8cdfc9303
RanulJaya <ranul.jaya@gmail.com>
2021-03-19 19:04:21 +1300

    new files added

---
+++ Python/Birth.py
@@ -0,0 +1,4 @@
+birth_year = input('Birth year: ') #This will automatically store a string not an int
+age = 2019 - int(birth_year)
+
+print(age)
---
+++ Python/inputfunction.py
@@ -0,0 +1,5 @@
+name = input('What is your name? ')
+print('Hi ', name)
+
+user_color = input('What is your faviourite color?')
+print('His faviourite color is ' + user_color)

f58d535259a8b0f48756eb5bcc683fcde0d2acda
RanulJaya <ranul.jaya@gmail.com>
2021-03-20 12:23:45 +1300

    operations in python

---
+++ Python/logicalOperators.py
@@ -0,0 +1,9 @@
+has_high_income = True
+has_good_credit = False
+
+#if both are true, it will print the line as both conditions do meet
+if has_good_credit and has_good_credit: #if one condition is true and the other is false then there will be not text since the conditions don't meet
+    print('Eligible for a loan')
+#and: both
+#or:at least one
+#not: changes the boolean to the opposite

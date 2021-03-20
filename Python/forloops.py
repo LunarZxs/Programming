b49224d680ddd68f8d50b0510b923ada67bda751
RanulJaya <ranul.jaya@gmail.com>
2021-03-20 14:28:52 +1300

    adding for loops

---
+++ Python/forloops.py
@@ -0,0 +1,14 @@
+#iterate over a items of a collection
+#item is a loop variable
+
+for item in 'Python':
+    print(item)
+
+for item in ['Ranul', 'Jaya']:  
+    #[] used to iterate over a list          
+    print(item)
+    #prints words in a line and creates a new line for the new word
+
+for item in range(10):
+    print(item)
+#goes from 0 to 9

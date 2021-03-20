8e4f083dc5dec0edffcb16a86581dd2d5e470839
RanulJaya <ranul.jaya@gmail.com>
2021-03-20 15:12:54 +1300

    adding variable file

---
+++ JavaScript/Node.js/variable.js
@@ -0,0 +1,9 @@
+//Explaining DataTypes
+
+var myName = "Ranul" //var can be set to anything which will be used through out the program
+
+myName = 8
+
+let ourName = "jobspy" //anther way to set variables and will be used in the scope 
+
+const pi = 3.14 //cannot be changed

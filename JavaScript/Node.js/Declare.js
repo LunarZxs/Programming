6f5181e1cff1d686dc7f8be5bcd7d645b81cbb06
RanulJaya <ranul.jaya@gmail.com>
2021-03-20 16:01:17 +1300

    Declare.js explains var

---
+++ JavaScript/Node.js/Declare.js
@@ -0,0 +1,12 @@
+var product = 2.0 * 2.5; //prints 5
+
+console.log(product);
+
+var remainder;
+remainder = 11 % 3; // shows a remainder
+
+var a = 3;
+a += 12; //compound assigment with augmented addition
+
+a-=11; //compound assignment with augmented subtraction
+

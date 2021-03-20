9b84b38f8e57bbd65163b845a46a127dc2ddc2fb
RanulJaya <ranul.jaya@gmail.com>
2021-03-20 16:15:57 +1300

    commiting double quotes

---
+++ JavaScript/Node.js/doublequote.js
@@ -0,0 +1,4 @@
+var myStr = "I am a \"Double quoted\"\ double quotations";
+//This backslash is used to remove the issue with quotations
+//Single quotes are easier to remove the issue
+console.log(myStr)

4ff0ef328f882ed140b71cc2094239a75016d141
RanulJaya <ranul.jaya@gmail.com>
2021-03-20 15:15:01 +1300

    ammended changes

--- JavaScript/Node.js/variable.js
+++ JavaScript/Node.js/variable.js
@@ -1,9 +1,9 @@
 //Explaining DataTypes
 
-var myName = "Ranul" //var can be set to anything which will be used through out the program
+var myName = "Ranul"; //var can be set to anything which will be used through out the program
 
-myName = 8
+myName = 8;
 
-let ourName = "jobspy" //anther way to set variables and will be used in the scope 
+let ourName = "jobspy"; //anther way to set variables and will be used in the scope 
 
-const pi = 3.14 //cannot be changed
+const pi = 3.14; //cannot be changed

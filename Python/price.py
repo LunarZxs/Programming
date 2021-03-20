abca3d46edaf26f1bcd7ec53fe327cf95b81f439
RanulJaya <ranul.jaya@gmail.com>
2021-03-20 14:34:27 +1300

    prices simulator

---
+++ Python/price.py
@@ -0,0 +1,16 @@
+#prices[10,20,30] prints 60
+prices = 0
+
+for items in [10,20,30]:
+    prices += items
+
+print(prices)
+
+#below is the second method
+price_items = [10,20,30]
+total = 0
+
+for price in price_items:
+    total += price
+
+print(f'Total: {total}')

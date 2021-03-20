99cce1fe1396e3f9d9f78fa94906ec2f72bfbf7c
RanulJaya <ranul.jaya@gmail.com>
2021-03-20 16:21:57 +1300

    adding changes to the doublequote.js file

--- JavaScript/Node.js/doublequote.js
+++ JavaScript/Node.js/doublequote.js
@@ -2,3 +2,7 @@ var myStr = "I am a \"Double quoted\"\ double quotations";
 //This backslash is used to remove the issue with quotations
 //Single quotes are easier to remove the issue
 console.log(myStr)
+
+var concatString = "I come first" + " I come second";
+//The top code is to concat two strings
+console.log(concatString)

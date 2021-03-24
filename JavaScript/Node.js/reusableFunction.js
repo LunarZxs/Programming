28a89097c8d1a904852791306aad520d61660bb0
RanulJaya <ranul.jaya@gmail.com>
2021-03-25 10:50:34 +1300

    added new function to the resusableFunction

---
+++ JavaScript/Node.js/reusableFunction.js
@@ -0,0 +1,14 @@
+function ourReusableFunction(){
+    console.log("Hello World") //when the function is run or invoked then this will print
+}
+
+ourReusableFunction();
+
+function resueFunction(a, b)
+{
+    c = a + b;
+
+    return c;
+}
+
+console.log(resueFunction(5,7));

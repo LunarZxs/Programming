0ac0b6d1e3f6af96c2d237299898a083fdf92638
RanulJaya <ranul.jaya@gmail.com>
2021-03-25 13:34:36 +1300

    pushing the boolean js

---
+++ JavaScript/Node.js/Boolean.js
@@ -0,0 +1,16 @@
+function welcomeToBooleans(isTrue)
+{
+    if(isTrue){
+
+        return "This is the return string";
+        
+    }
+
+    else{
+        return "This is the false string"
+    }
+}
+
+statement = welcomeToBooleans(true);
+
+console.log(statement)

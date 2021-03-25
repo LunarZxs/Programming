7abe44abfe5480f1044bd2f621164e6615446d3b
RanulJaya <ranul.jaya@gmail.com>
2021-03-25 20:34:17 +1300

    case.js new cases added

--- JavaScript/Node.js/case.js
+++ JavaScript/Node.js/case.js
@@ -4,20 +4,52 @@ function caseInSwitch(val)
 
     switch(val){
         case 1:
-            ans = "alpha";
-        
+            ans = "alpha"; //without breack statement, it will follow through to the next case
+            
         case 2:
             ans = "beta";
+            break;
                 
         case 3:
             ans = "gamma";
+            break;
         
         case 4:
             ans = "delta";
+            break;
 
     }
 
     return ans;
 }
 
+function caseNewInSwitch(val)
+{
+    var ans = "";
+
+    switch(val){
+        case "a":
+            ans = "alpha";
+            break;
+        
+        case "b":
+            ans = "beta";
+            break;
+                
+        case "c":
+            ans = "gamma";
+            break;
+        default: //if conditions don't meet, this line is executed
+            ans = "stuff";
+            break;
+        
+        return ans;
+    }
+
+    return ans;
+}
+
+
 console.log(caseInSwitch(1));
+
+console.log(caseNewInSwitch(1));

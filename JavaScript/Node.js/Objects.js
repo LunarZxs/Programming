688bb7bfe81223601b456b496fd2f96d89dae9a9
RanulJaya <ranul.jaya@gmail.com>
2021-03-25 23:56:04 +1300

    new changes made to the object file

---
+++ JavaScript/Node.js/Objects.js
@@ -0,0 +1,10 @@
+var obj = {
+    "name": "Ranul",
+    "age" : 12,
+    "Hobbies" : "Testing"
+};
+
+var personAge = obj["name"]; //prints the property of the variable
+obj.name = "newName" //setting a new variable for the property
+
+console.log(obj["name"]); //name updated

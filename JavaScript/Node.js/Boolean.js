a46f2ed146711e3ee418c5555da232fa06cddcd7
RanulJaya <ranul.jaya@gmail.com>
2021-03-25 13:39:27 +1300

    change in the Boolean.js file

--- JavaScript/Node.js/Boolean.js
+++ JavaScript/Node.js/Boolean.js
@@ -2,7 +2,7 @@ function welcomeToBooleans(isTrue)
 {
     if(isTrue){
 
-        return "This is the return string";
+        return "This is the return string for true";
         
     }
 
@@ -10,6 +10,19 @@ function welcomeToBooleans(isTrue)
         return "This is the false string"
     }
 }
+
+function testingTrue(checkValue)
+{
+    if(checkValue == 1){
+        return "True"
+    }
+
+    else{
+        return "false"
+    }
+}
+
+console.log(testingTrue(2))
 
 statement = welcomeToBooleans(true);
 

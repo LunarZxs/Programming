3230eeaa1edd9d0e1da9587b9b7c20b5bead86d2
RanulJaya <ranul.jaya@gmail.com>
2021-03-26 23:02:39 +1300

    nestedobj updated

---
+++ JavaScript/Node.js/NestedObj.js
@@ -0,0 +1,44 @@
+var myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps", 
+            "passenger seat": "crumbs"
+        }, 
+        "outside": {
+            "trunk": "jacks"
+        }
+    },
+
+    "truck": {
+        "inside": {
+            "glove box" : "maps"
+            ,"passenger seat": "crumbs"
+        }, 
+
+        "outside": {
+            "trunk": "jacks"
+        }
+    }
+};
+
+var myNewStorage = 
+[{ //have to access using array index        
+        type: "car",
+        list: [
+            "glove box",
+            "passenger seat"
+        ],
+    },
+    {
+        type: "truck",
+        list: [
+            "glove box",
+            "passenger seat"
+        ]
+    }
+    
+]; //use square brackets
+
+console.log(myStorage["car"]["inside"]["glove box"]) //nested loops can be accesed through arrays
+console.log(myStorage.car.inside["glove box"]); //must be in array to access the property otherwise an error can occur
+console.log(myNewStorage[0].list[0]) //prints glove box from truck type

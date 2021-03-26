2d647b2557b4fdb67b90cc6f5bfa2f600a2077d1
RanulJaya <ranul.jaya@gmail.com>
2021-03-26 20:13:13 +1300

    objects file

--- JavaScript/Node.js/Objects.js
+++ JavaScript/Node.js/Objects.js
@@ -8,3 +8,6 @@ var personAge = obj["name"]; //prints the property of the variable
 obj.name = "newName" //setting a new variable for the property
 
 console.log(obj["name"]); //name updated
+obj["newProp"] = "Hello"; //adding a property 
+
+console.log(obj);

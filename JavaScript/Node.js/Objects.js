4f9b7ea8b26433e2e7734938a58b2cb5048de040
RanulJaya <ranul.jaya@gmail.com>
2021-03-26 20:21:39 +1300

    put a check function

--- JavaScript/Node.js/Objects.js
+++ JavaScript/Node.js/Objects.js
@@ -10,4 +10,19 @@ obj.name = "newName" //setting a new variable for the property
 console.log(obj["name"]); //name updated
 obj["newProp"] = "Hello"; //adding a property 
 
-console.log(obj);
+console.log(obj); //prints all the objects in the variable
+
+delete obj.Hobbies; //deletes the hobbies property
+
+function checkObj(checkProp) //check if property exists
+{
+    if(obj.hasOwnProperty(checkProp))
+    {
+        return obj[checkProp];
+    }
+    else{
+        return "Not Found";
+    }
+}
+
+console.log(checkObj("name")); //prints the checked object

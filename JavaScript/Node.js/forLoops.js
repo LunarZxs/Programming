74949f99db39067c27c5bf9897c3082df6edfc5a
RanulJaya <ranul.jaya@gmail.com>
2021-03-27 13:38:58 +1300

    forLoops js program

---
+++ JavaScript/Node.js/forLoops.js
@@ -0,0 +1,25 @@
+var ourArray = [];
+
+for(var i = 0; i < 5; i++)
+{
+    ourArray.push(i);
+}
+
+console.log(ourArray);
+
+for(var i = 10; i >0; i--)
+{
+    ourArray.push(i);
+}
+
+console.log(ourArray);
+
+var myArr = [2,3,4,5,6];
+var total = 0;
+
+for(var i = 0; i < myArr.length; i++)
+{
+    total += myArr[i];
+}
+
+console.log(total);

a3204a6ee7c3f9ef9a0a5bea49f2f988665ec562
RanulJaya <ranul.jaya@gmail.com>
2021-03-28 15:33:07 +1300

    datatypes

--- JavaScript/Node.js/variable.js
+++ JavaScript/Node.js/variable.js
@@ -5,5 +5,7 @@ var myName = "Ranul"; //var can be set to anything which will be used through ou
 myName = 8;
 
 let ourName = "jobspy"; //anther way to set variables and will be used in the scope 
+let ourName = "hello"; //deosn't allow the array to be declared twice when let is being used
 
+"use strict" //used for unsafe actions or coding mistakes
 const pi = 3.14; //cannot be changed

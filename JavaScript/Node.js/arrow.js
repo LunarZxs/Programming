6b25b31b3d7e7d0cc1e65ae454bd9d3a7b8c6fde
RanulJaya <ranul.jaya@gmail.com>
2021-03-28 15:47:01 +1300

    arrow function

---
+++ JavaScript/Node.js/arrow.js
@@ -0,0 +1,6 @@
+var magic = () => new Date(); //This returns the date and shorten version of return
+
+
+var myConcat = (arr1, arr2) => arr1.concat(arr2); //Passing in two arguements and return the function 
+
+console.log(myConcat([1,2], [3,4,5]));

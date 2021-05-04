f9e57d63030a358ba448255e45b0d1e66b58a292
RanulJaya <ranul.jaya@gmail.com>
2021-05-04 19:12:30 +1200

    to-do-app test

---
+++ To-do-App/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get()
+
+app.listen(8000, () =>{
+    console.log('Port listening to port 8000')
+})
---
+++ To-do-App/module.js
@@ -0,0 +1,8 @@
+function myfunction(){
+    console.log('function was called')
+}
+
+var myStriing = 'String'
+
+module.exports.myfunction = myfunction
+module.exports.myStriing = "String"
